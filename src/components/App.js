6fb52520a01c4a49fa4f4ed2f90caa2b373958fa
GainorB <gainorbostwick@gmail.com>
2018-05-06 20:12:16 -0400

    basic styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Navbar from './Navbar/Navbar';
 import Search from './Search/Search';
+import '../../static/css/style.css';
 
 const App = () => (
   <MuiThemeProvider>
